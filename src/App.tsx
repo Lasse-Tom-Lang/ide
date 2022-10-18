c95cab8163f0a9173ab96a8a0f672ff09f45725b
Lasse Lang <lang.lasse@gmx.de>
2022-10-18 18:49:32 +0200

    Added first boilerplait

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./style.scss"
+
+const App: React.FC = () => {
+
+  return (
+    <>
+      <nav>
+
+      </nav>
+      <div id="filesystem">
+
+      </div>
+      <main contentEditable="true">
+        A
+      </main>
+    </>
+  )
+}
+
+export default App
