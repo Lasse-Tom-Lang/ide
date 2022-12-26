3c6427866aceff116f37526865474989bd85f038
Lasse Lang <lang.lasse@gmx.de>
2022-12-26 21:56:55 +0100

    Tested some syntax highlighting

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -21,13 +21,20 @@ const Main: React.FC<MainProps> = (props) => {
         </div>
         <div className="textField" contentEditable={props.activeTab != "" ? "true" : "false"} spellCheck="false">
           {
-            props.file.toString()
-              .replace(/ /g, "\u00a0")
-              .replace(/[<]/g, "&lt;")
-              .replace(/[>]/g, "&gt;")
-              .split(/\n/).map((line) =>
-                <>{parse(line)}<br/></>
-              )
+            props.activeTab.split(".")[props.activeTab.split(".").length - 1] == "ts" ?
+              props.file.toString()
+                .replace(/ /g, "\u00a0")
+                .replace(/[<]/g, "&lt;")
+                .replace(/[>]/g, "&gt;")
+                .replace(/(let|var|const|interface)+\s+(\S*)+\s/g, "$1 <span style='color:red;'>$2</span> ")
+                .replace(/(interface |var |let |const |if|else|function)/g, "<span style='color:orange;'>$1</span>")
+                .replace(/({|}|\(|\))/g, "<span style='color:blue;'>$1</span>")
+                .replace(/("(.*?)")/g, "<span style='color:green'>$1</span>")
+                .split(/\n/).map((line) =>
+                  <>{parse(line)}<br/></>
+                )
+                : 
+                props.file.toString().split(/\n/).map((line) => <>{parse(line)}<br/></>)
           }
         </div>
     </main>
